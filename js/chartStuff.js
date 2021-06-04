e2411544fe67b59fac9083d87434d6d877a68b01
juhamikael <83360104+juhamikael@users.noreply.github.com>
2021-06-04 12:59:47 +0300

    Delete chartStuff.js

--- js/chartStuff.js
+++
@@ -1,6 +0,0 @@
-export function destroyChart() {
-  canvasElement.remove();
-  const newCanvas = document.createElement("canvas");
-  canvasDiv.appendChild(newCanvas);
-  canvasElement = newCanvas;
-}
